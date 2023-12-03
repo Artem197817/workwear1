927576d9a6519e07c927c7979a6d345aa1b71800
Artem <katod1978@yandex.ru>
2023-12-03 13:00:40 +0700

    Modul ClientServer POST

--- src/main/java/demo/workwear/model/Employee.java
+++ src/main/java/demo/workwear/model/Employee.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,11 +23,15 @@ public class Employee {
     @Id
     @GeneratedValue
     private Long id;
+    @NotBlank
     private String firstName;
+    @NotBlank
     private String lastName;
+    @NotBlank
     private String patronymic;
     private ProductionDivision productionDivision;
     private Company company;
+    @NotBlank
     private String specialization;
 
     public Employee(String firstName, String lastName, String patronymic,
